bd2dfc6d739757509093517a884ca0da3d9988c3
Abdullah <mdabdullahnoman777@gmail.com>
2022-05-22 17:55:25 +0600

    Server Run succesfully

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
+app.use(express.json());
+app.use(cors());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.imtr4p9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollections = client.db("manufacturer").collection("product");
+    const reviewsCollections = client.db("manufacturer").collection("reviews");
+    app.get("/products", async (req, res) => {
+      const query = req.body;
+      const products = await productCollections.find(query).toArray();
+      res.send(products);
+    });
+
+    // Reviews
+    app.get("/review", async (req, res) => {
+      const query = req.body;
+      const review = await reviewsCollections.find(query).toArray();
+      res.send(review);
+    });
+  } finally {
+  }
+}
+
+run().catch;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
