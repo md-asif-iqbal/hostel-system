d95455287e082292edad0400219c442e6104763b
Abdullah <mdabdullahnoman777@gmail.com>
2022-05-22 18:18:32 +0600

    Error Issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-require("dotenv").config();
+require("dotenv").config()
 app.use(express.json());
 app.use(cors());
 
