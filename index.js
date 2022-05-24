f6f3127e4cfd1da96f7d5d399f33352ec09d3f2e
Abdullah <mdabdullahnoman777@gmail.com>
2022-05-24 12:23:46 +0600

    Verify admin

--- index.js
+++ index.js
@@ -156,7 +156,20 @@ async function run() {
     });
 
     // Admin Api
-    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await usersCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    };
+
+    app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
       const filter = { email: email };
       const updateDoc = {
